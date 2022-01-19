c6a7d0b5a42977c407bc892735d3e4c19441a451
LuannMateus <luanmateuslima34@gmail.com>
2022-01-19 19:14:19 -0300

    Add NavLinks with unit tests

---
+++ frontend/src/components/NavLinks/styles.js
@@ -0,0 +1,15 @@
+import styled, { css } from 'styled-components';
+
+export const Container = styled.div`
+  ${({ theme }) => css`
+    display: flex;
+    flex-flow: row wrap;
+
+    background: #fff;
+
+    @media ${theme.media.lteMedium} {
+      flex-flow: column wrap;
+      align-content: center;
+    }
+  `}
+`;
