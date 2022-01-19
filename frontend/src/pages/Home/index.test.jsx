5812d567082622ef26c538dcd8ef793cd43e16ab
LuannMateus <luanmateuslima34@gmail.com>
2022-01-18 23:39:01 -0300

    Add Heading unit tests

--- frontend/src/pages/Home/index.test.jsx
+++ frontend/src/pages/Home/index.test.jsx
@@ -1,18 +1,8 @@
-import { render, screen } from '@testing-library/react';
 import { Home } from '.';
 import { renderTheme } from '../../styles/renderTheme';
-import { theme } from '../../styles/theme';
 
-test('renders learn react link', () => {
-  const { debug } = renderTheme(<Home />);
-
-  const headingContainer = screen.getByRole('heading', {
-    name: 'Hello',
-  }).parentElement;
-
-  expect(headingContainer).toHaveStyle({
-    background: theme.colors.BG__MAIN,
+describe('', () => {
+  it('Should render Home', () => {
+    renderTheme(<Home />);
   });
-  expect(headingContainer).toMatchSnapshot();
-  expect(headingContainer).toHaveStyleRule('background', 'red');
 });
