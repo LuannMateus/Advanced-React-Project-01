12398f3c29284b0c2fd287cd7e5e4935d1aa898d
LuannMateus <luanmateuslima34@gmail.com>
2022-01-28 20:46:03 -0300

    Change database env config

--- backend/config/database.js
+++ backend/config/database.js
@@ -4,12 +4,12 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'mongoose',
       settings: {
-        host: env('DATABASE_HOST', 'cluster-1.xsv4h.mongodb.net'),
+        host: env('DATABASE_HOST', ''),
         srv: env.bool('DATABASE_SRV', true),
         port: env.int('DATABASE_PORT', 27017),
-        database: env('DATABASE_NAME', 'projectDb'),
-        username: env('DATABASE_USERNAME', 'dev'),
-        password: env('DATABASE_PASSWORD', 'dev'),
+        database: env('DATABASE_NAME', ''),
+        username: env('DATABASE_USERNAME', ''),
+        password: env('DATABASE_PASSWORD', ''),
       },
       options: {
         authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
